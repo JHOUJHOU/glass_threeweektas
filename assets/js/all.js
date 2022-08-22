e552d768da3f025b5ed1ca11054b9be20ce53107
JHOUJHOU <a01450887@gmail.com>
2022-08-23 00:52:15 +0800

    Update 2022-08-22T16:52:11.793Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,15 +1,2 @@
 "use strict";
-
-var menu = document.querySelector("hamburger");
-var bsCollapse = new Collapse(menu, {
-  toggle: false
-});
-
-function closeNavHam() {
-  bsCollapse.hide();
-}
-
-function toggleNavHam() {
-  bsCollapse.toggle();
-}
 //# sourceMappingURL=all.js.map
